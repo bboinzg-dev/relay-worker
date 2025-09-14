abeaa10f9b2901a45bbd2c554e067496ac1bcd03
bboinzg-dev <bboinzg@gmail.com>
2025-09-15 06:51:33 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authRouter from "./routes/auth.js";
+import authRouter from "./routes/manager.js"; // ← 여기만 바꾸기
 
 const app = express();
 app.use(express.json());

adf7bda093a6b6718153b824963e0ed491e4c3a4
bboinzg-dev <bboinzg@gmail.com>
2025-10-03 10:03:34 +0900

    131

--- test_import.cjs
+++
@@ -1,3 +0,0 @@
-const { batchProcess } = require('./docai_client.cjs');
-const { extractRelayFields } = require('./docai_parse.cjs');
-console.log('OK:', typeof batchProcess, typeof extractRelayFields);

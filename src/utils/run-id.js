036c285950fd9423cadef0d71da2de9e58aa5cc1
bboinzg-dev <bboinzg@gmail.com>
2025-10-06 21:36:22 +0900

    24

--- src/utils/run-id.js
+++ src/utils/run-id.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const crypto = require('crypto');
+const { randomUUID } = require('node:crypto');
 
-function generateRunId() {
-  if (typeof crypto.randomUUID === 'function') return crypto.randomUUID();
-  return crypto.randomBytes(16).toString('hex');
+function generateRunId(prefix = '') {
+  // 기본은 uuid v4. 필요하면 prefix 붙여서 사용 가능.
+  const id = randomUUID();
+  return prefix ? `${prefix}_${id}` : id;
 }
 
 module.exports = { generateRunId };

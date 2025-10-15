7c69b2e6cb4217e13d7c5d279a50b13ef3655ec2
bboinzg-dev <bboinzg@gmail.com>
2025-10-15 17:05:08 +0900

    Create blueprint.js

---
+++ src/types/blueprint.js
@@ -0,0 +1,6 @@
+// @ts-check
+
+/** @type {readonly ['length_mm','width_mm','height_mm','dim_l_mm','dim_w_mm','dim_h_mm']} */
+const DIM_KEYS = ['length_mm','width_mm','height_mm','dim_l_mm','dim_w_mm','dim_h_mm'];
+
+module.exports = { DIM_KEYS };
